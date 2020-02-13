88ff12867af03a9956c07843ffe048877bd7aa7b
Pei <stevenkaoo@gmail.com>
2020-02-13 22:09:11 +1100

    update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         {/* <GithubUserInfo username="Elie"/> */}
-        {/* <Deck /> */}
-        <JokeList />
+        <Deck />
+        {/* <JokeList /> */}
       </div>
     );
   }
